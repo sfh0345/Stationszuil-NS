b994acf1ad962f92e3f6fb1be4190248db74d11a
sfh0345 <sander.holl@student.hu.nl>
2023-09-11 15:07:06 +0200

    test commit

---
+++ main.py
@@ -0,0 +1,17 @@
+print('Enter your name:')
+name = input()
+
+print('Enter your age')
+leeftijd = input()
+
+pluseen = int(leeftijd) + 1
+print('Hoi, ' + name + ' je bent over een jaar ' + str(pluseen))
+#leeftijd plus 1
+
+leeftijdmax = 18
+if int(leeftijd) >= leeftijdmax:
+  print("Je mag rijden")
+else:
+  print("Je mag niet rijden")
+  #if statement of je boven of gelijk aan 18 bent
+
