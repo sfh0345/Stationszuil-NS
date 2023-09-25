2e4416fe74dfa0f46bd0da527c55e718fa0a63d0
sfh0345 <sander.holl@student.hu.nl>
2023-09-25 15:19:54 +0200

    Moderation file nu opnieuw gemaakt zonder chatgpt

--- test.py
+++ test.py
@@ -1,69 +1,160 @@
+import csv
 import os
+#inport csv file generator
 
-# Functie om feedback te lezen uit het bestand
-def lees_feedback(bestandsnaam):
-    feedback = []
-    if os.path.exists(bestandsnaam):
-        with open(bestandsnaam, 'r') as bestand:
-            feedbacktekst = bestand.readlines()
-            for blok in feedbacktekst:
-                feedback.append(blok.strip())
-    return feedback
+from datetime import datetime
+now = datetime.now()
+datummod = now.strftime("%d/%m/%Y %H:%M:%S")
+#datum in een variable
 
-# Functie om feedback te schrijven naar geaccepteerd of afgewezen bestand
-def schrijf_feedback(bestandsnaam, feedback, status):
-    with open(bestandsnaam, 'a') as bestand:
-        for bericht in feedback:
-            bestand.write(bericht + "\n")
-        bestand.write("Status: " + status + "\n" + "-------------------------------------------------------------\n")
+print("Hallo, welkom op het moderatiedashboard")
 
-# Functie om feedback te verwijderen uit het bronbestand
-def verwijder_feedback(bestandsnaam, start, einde):
-    if os.path.exists(bestandsnaam):
-        with open(bestandsnaam, 'r') as bestand:
-            lijnen = bestand.readlines()
-        with open(bestandsnaam, 'w') as bestand:
-            for i, lijn in enumerate(lijnen):
-                if i < start or i > einde:
-                    bestand.write(lijn)
-
-def main():
-    print("Welkom bij het feedback moderatiedashboard")
-
-    naam = input("Voer uw naam in: ")
-    email = input("Voer uw e-mailadres in: ")
-
-    bestandsnaam = "input.csv"  # Veronderstel dat feedback in een tekstbestand zit
-
-    # Lees feedback uit het bestand in blokken van 7 regels
-    feedbacktekst = lees_feedback(bestandsnaam)
-    index = 0
-
-    while index < len(feedbacktekst):
-        blok = feedbacktekst[index:index+7]
-        print("\n".join(blok))
-        antwoord = input("Wilt u deze feedback accepteren? (ja/nee): ").lower()
-
-        if antwoord == "ja":
-            # Voeg de geaccepteerde feedback toe aan het geaccepteerd bestand
-            schrijf_feedback("geaccepteerd_feedback.csv", blok, "geaccepteerd")
-        elif antwoord == "nee":
-            # Voeg de afgewezen feedback toe aan het afgewezen bestand
-            schrijf_feedback("afgewezen_feedback.csv", blok, "afgewezen")
-        else:
-            print("Ongeldige invoer. Voer 'ja' of 'nee' in.")
+def naaminput():
+    while True:
+        naam = str(input("Naam?: "))
+        if len(naam) <= 0 or naam == " ":
+            print("U heeft geen naam opgegeven.")
             break
+        else:
+            return naam
 
-        # Verwijder de feedback uit het bronbestand
-        start = index
-        einde = index + 6
-        verwijder_feedback(bestandsnaam, start, einde)
+# Sla de userinput op
+naaminput = naaminput()
 
-        # Ga door naar het volgende blok van 7 regels
-        index += 7
+def emailinput():
+    while True:
+        email = str(input("Email: "))
+        if len(email) <= 0:
+            print("Er staat niks in je bericht.")
+        else:
+            return email
 
 
-    print("Alle feedback is verwerkt.")
 
-if __name__ == "__main__":
-    main()
+# Sla de bericht input op
+emailinput = emailinput()
+
+csv_file_path = 'input.csv'
+csv_file_geaccepteerd = 'geaccepteerd.csv'
+csv_file_afgewezen = 'afgewezen.csv'
+#maak een file aan
+
+continue_review = True
+
+while continue_review:
+    if os.path.getsize(csv_file_path) == 0:
+        print("U heeft alle feedback verwerkt.")
+        nothinginfile = true
+    else:
+        try:
+            with open(csv_file_path, 'r') as infile:
+                # Read the first 7 lines of the file
+                lines = infile.readlines()[:7]  # Slice the list to get the first 7 lines
+
+                # Iterate over the lines and print them
+                for line in lines:
+                    print(line.strip())  # Strip to remove newline characters
+                geaccepteerd = str(input("Wil je deze feedback accepteren? (ja/nee) ")).lower()
+                if geaccepteerd == "ja":
+
+
+                    # Schrijf variabelen weg in een csv file
+                    with open(csv_file_geaccepteerd, 'a', newline='') as file:
+                        writer = csv.writer(file)
+                        for line in lines:
+                            writer.writerow([line.strip()])
+
+                        writer.writerow(["Administrator moderation dashboard logs"])
+                        writer.writerow(["Naam: " + naaminput])
+                        writer.writerow(["Email: " + emailinput])
+                        writer.writerow(["Datum: " + datummod])
+                        writer.writerow(["-------------------------------------------------------------"])
+                        writer.writerow(["Status: Geaccepteerd"])
+                        writer.writerow(["-------------------------------------------------------------"])
+                        #variabelen zijn in de tekstfile gezet
+
+                    # verwijder
+                    with open(csv_file_path, 'r') as file:
+                        lines = file.readlines()
+                    # verwijder 7 lines
+                    updated_lines = lines[7:]
+                    # schrijf de lines terug die geupdate waren
+                    with open(csv_file_path, 'w') as file:
+                        file.writelines(updated_lines)
+
+
+                    print("Je hebt deze feedback geaccepteerd")
+
+
+                elif geaccepteerd == "nee":
+
+                    # Schrijf variabelen weg in een csv file
+                    with open(csv_file_afgewezen, 'a', newline='') as file:
+                        writer = csv.writer(file)
+                        for line in lines:
+                            writer.writerow([line.strip()])
+
+                        writer.writerow(["Administrator moderation dashboard logs"])
+                        writer.writerow(["Naam: " + naaminput])
+                        writer.writerow(["Email: " + emailinput])
+                        writer.writerow(["Datum: " + datummod])
+                        writer.writerow(["-------------------------------------------------------------"])
+                        writer.writerow(["Status: Afgewezen"])
+                        writer.writerow(["-------------------------------------------------------------"])
+                        # variabelen zijn in de tekstfile gezet
+
+                    # verwijder
+                    with open(csv_file_path, 'r') as file:
+                        lines = file.readlines()
+                    # verwijder 7 lines
+                    updated_lines = lines[7:]
+                    # schrijf de lines terug die geupdate waren
+                    with open(csv_file_path, 'w') as file:
+                        file.writelines(updated_lines)
+
+                    print("Je hebt deze feedback geaccepteerd")
+                else:
+                    print("Ongeldig antwoord (ja/nee)")
+
+
+        except FileNotFoundError:
+            pass
+    if nothinginfile == false
+        another_feedback = str(input("Wilt u nog een bericht reviewen? (ja/nee) ")).lower()
+        if another_feedback == "nee":
+            continue_review = False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # #als de file niet word gevonden gaat hij gewoon door met de code
+    #
+    # # Schrijf variabelen weg in een csv file
+    # with open(csv_file_path, 'a', newline='') as file:
+    #     writer = csv.writer(file)
+    #     writer.writerow([" "])
+    #     writer.writerow(["-------------------------------------------------------------"])
+    #     writer.writerow(["Een gebruiker heeft feedback achtergelaten - " + datum])
+    #     writer.writerow(["Naam: " + tekstinput])
+    #     writer.writerow(["Bericht: " + berichtinput])
+    #     writer.writerow(["Locatie: " + station])
+    #     writer.writerow(["-------------------------------------------------------------"])
+    #     #variabelen zijn in de tekstfile gezet
+    #
+    # print("Bedankt voor uw feedback!")
+    # #Final message sturen
+
+
+
+
+
